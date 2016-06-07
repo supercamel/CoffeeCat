48105b9c5203326bf63db3994f35f81e07f7f452
Samuel Cowen <samuel.cowen@camelsoftware.com>
2016-06-07 21:21:48 +1000

    Added stdlib

---
+++ examples/gregory-leibniz/stdlib/print.h
@@ -0,0 +1,32 @@
+#ifndef COFFEECAT_PRINT_H
+#define COFFEECAT_PRINT_H
+
+#include <etk/etk.h>
+#include <iostream>
+
+constexpr uint32 max_line_length = 128;
+
+
+class Printer : public etk::Stream<Printer>
+{
+public:
+    void put(char c)
+    {
+        std::cout << c << std::flush;
+    }
+    
+
+    
+};
+
+extern Printer io;
+
+inline void print(auto t)
+{
+    io.print(t);
+}
+
+
+
+#endif
+
---
+++ examples/inheritance/stdlib/print.cpp
@@ -0,0 +1,4 @@
+#include "print.h"
+
+Printer io;
+
